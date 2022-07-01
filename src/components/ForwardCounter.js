06be30e3d63481d9dc9579e5457af5119db52fca
Jeff Ripke <jripke74@gmail.com>
2022-07-01 10:42:27 -0700

    add custom hook use-counter in components BackwardCounter and ForwardCounter

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
-
 import Card from './Card';
+import useCounter from '../hooks/use-counter';
 
 const ForwardCounter = () => {
-  
-
+  const counter = useCounter();
   return <Card>{counter}</Card>;
 };
 
