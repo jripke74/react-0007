084e5a9cac7096551fc0880cbe9c004795aede6a
Jeff Ripke <jripke74@gmail.com>
2022-07-01 09:26:12 -0700

    add hooks/use-counter.js and initial commit

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+import Card from './Card';
+
+const ForwardCounter = () => {
+  
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
